a9a84687bf594ba64c3e384a52a6ce8d989ca199
saadshaikh786 <shaikhsaaduddin56@eng.rizvi.edu.in>
2025-10-06 16:22:32 +0530

    Add files via upload

---
+++ EXPERIMENT3.C
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+/* NAME : Saad shekh
+   UNI  : 251C036
+   FLIDE: CIVIL
+   DIV  : B */
+
+#include <stdio.h>
+
+int main()
+{
+    printf("\n saad shekh");
+    printf("\n i am 19");
+    printf("\n i am from class civil");
+    printf("\n i am 1st year");
+    printf("\n division B");
+    printf("\n UNI no 251C036");
+
+    return 0;
+}

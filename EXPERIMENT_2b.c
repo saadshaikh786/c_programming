cc48611faee945340452a2227565c1a15b768b51
saadshaikh786 <shaikhsaaduddin56@eng.rizvi.edu.in>
2025-10-10 14:12:53 +0530

    Update and rename EXPERIMENT1.c to EXPERIMENT_2b.c

---
+++ EXPERIMENT_2b.c
@@ -0,0 +1,21 @@
+/* NAME : SAAD SHEKH
+   UNI  : 251C036
+   FLIDE: CIVIL
+   DIV  : B */
+
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("enter a number:");
+    scanf("%d", &num);
+    
+    if(num % 2 == 0) {
+        printf("%d is an even number.\n", num);
+    } else {
+        printf("%d is an odd number.\n", num);
+    }
+
+    return 0;
+}
